05c94ef89006f8340e4d9268d6aa31e72de281d3
OTA Hiroshi <ota_h@nifty.com>
2014-02-16 07:01:03 +0900

    adds reading config and bind to config

--- lib/dns_workflow/config.rb
+++ lib/dns_workflow/config.rb
@@ -5,14 +5,20 @@ module DNSWF
   # Represents configuration
   class Config
     @target_zone = 'example.zone'
-
-    def initialize(args = {})
-      config = read_config(args)
-      @target_zone =
-        config['target_zone'] if config && config['target_zone']
-    end
+    @target_zone_files = 'example.zone, example.rev'
+    @target_conf_files = 'named.conf'
 
     attr_reader :target_zone
+    attr_reader :target_zone_files
+    attr_reader :target_conf_files
+
+    # TODO: DI is needed
+    def initialize(args = {})
+      config = read_config(args)
+      config.each do |k, v|
+        instance_variable_set("@#{k}", v)
+      end if config
+    end
 
     def read_config(args = {})
       if args[:yaml] && File.exist?(args[:yaml])
